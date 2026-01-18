ed7affd758756a474e6174e87b78b3f7f2f3c858
Serhei <s.chikita@mail.ru>
2026-01-18 22:05:24 +0300

    create proj

--- web/dashboard/lib/api.js
+++ web/dashboard/lib/api.js
@@ -3,7 +3,7 @@ const BASE = process.env.NEXT_PUBLIC_ANALYTICS_URL || "http://localhost:8004";
 export async function apiGet(path) {
     const res = await fetch(`${BASE}${path}`, {
         headers: {
-            "X-Internal-Token": "super_secret_token"
+            "Content-Type": "application/json",
         },
         cache: "no-store"
     });
